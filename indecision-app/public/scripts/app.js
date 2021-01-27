61ecaa24673c1e115b058c64fbe003ead1b268a2
Vladimir Stratiev <vladistratiev@vladis-mac.local>
2021-01-27 16:04:25 +0200

    Events and Attributes

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,57 +1,63 @@
 'use strict';
 
-// argument object - no longer bound with arrow functions
+console.log('App.js is Running!');
 
-// const add = function(a, b) {
-//     console.log(arguments)
-//     return a + b;
-// }
-var add = function add(a, b) {
-    //console.log(arguments); no longer used
-    return a + b;
-};
-console.log(add(55, 1, 1001));
+// const templateApp = (
+//     <div>
+//         <h1>The Movie is: {app.title}</h1> 
+//         <p>Subtitles: {app.subtitles}</p> 
+//         {app.subtitles && <p>{app.subtitles}</p>}
+//         <p>{app.options.length > 0 ? 'Here are your options' : 'No Options'}</p>
+//         <ol>
+//         <li>Item One</li>
+//         <li>Item Two</li>
+//         </ol>
+//     </div>);
 
-// this keyword - no longer bound
-var user = {
-    name: 'Vladimir',
-    cities: ['Varna', 'Alaska', 'London', 'Sofia'],
-    printPlacesLived: function printPlacesLived() {
-        var _this = this;
-
-        return this.cities.map(function (city) {
-            return _this.name + ' has lived in ' + city;
-        });
-        //printPlacesLived: function(){
-        // console.log(this.name);
-        // console.log(this.cities);
-        // const that = this; // using this as parents function
-
-        // this.cities.forEach(function(city){
-        //     console.log(that.name + ' has lived in ' +  city);
-        // });
-
-        // this.cities.forEach((city) => {
-        //     console.log(this.name + ' has lived in ' +  city);
-        // });
-    }
-};
-console.log(user.printPlacesLived());
-
-//
-var multiplier = {
-    // array of numbers
-    // multiplyBy - single number
-    // multiply - return a new array where the numbers have been multiplied
-    numbers: [10, 20, 30],
-    multiplyBy: 3,
-    multiply: function multiply() {
-        var _this2 = this;
-
-        return this.numbers.map(function (number) {
-            return number * _this2.multiplyBy;
-        });
-    }
+var count = 0;
+var addOne = function addOne() {
+    console.log('plusOne');
 };
 
-console.log(multiplier.multiply()); //[1,2,3] 2 [2,4,6]
+var minusOne = function minusOne() {
+    console.log('minusOne');
+};
+
+var reset = function reset() {
+    console.log('reset');
+};
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Count: ',
+        count
+    ),
+    React.createElement(
+        'button',
+        { onClick: addOne },
+        '+1'
+    ),
+    React.createElement(
+        'button',
+        { onClick: minusOne },
+        '-1'
+    ),
+    React.createElement(
+        'button',
+        { onClick: reset },
+        'Reset'
+    )
+);
+console.log(templateTwo);
+
+//create two new button
+// Make Button "-1" set up minusOne() and register - log 'minusOne'
+// Make Reset buttor "reset" - setup reset() log "reset"
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(templateTwo, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,74 +1,44 @@
 console.log('App.js is Running!');
 
-//JSX - JavaScript XML
-//var template = /*#__PURE__*/React.createElement("h1", {id: "1234"}, "Something New!");
-const templateInfo = (
-    <div>
-        <h1>Indecision App</h1> 
-        <p> This is Some info</p> 
-        <ol>
-        <li>Item One</li>
-        <li>Item Two</li>
-        </ol>
-    </div>);
+// const templateApp = (
+//     <div>
+//         <h1>The Movie is: {app.title}</h1> 
+//         <p>Subtitles: {app.subtitles}</p> 
+//         {app.subtitles && <p>{app.subtitles}</p>}
+//         <p>{app.options.length > 0 ? 'Here are your options' : 'No Options'}</p>
+//         <ol>
+//         <li>Item One</li>
+//         <li>Item Two</li>
+//         </ol>
+//     </div>);
 
-//Create templateTwo var JSX expression
-//div
-//  h1 --> Vladimir Stratiev
-//  p --> Age: 28
-//  p --> Location: Philadelphia
-//  Render templateTwo instead of template
-
-const user = {
-    userName: 'Vladimir Stratiev',
-    userAge: 28,
-    userLocation: 'Sofia'
+let count = 0;
+const addOne = () => {
+    console.log('plusOne');
 };
 
-// var userName = 'Vladimir Stratiev';
-// var userAge = 28;
-// var userLocation = 'Sofia';
+const minusOne = () => {
+    console.log('minusOne');
+};
 
-function getLocation(location){
-    if(location){
-        return <p>Location: {location} </p>;
-    }
-}
-const templateUser = (
+const reset = () => {
+    console.log('reset');
+};
+
+const templateTwo = (
     <div>
-    <h1>{user.userName ? user.userName : 'Anonymous'}</h1>
-    {(user.userAge && user.userAge >= 18) && <p>Age: {user.userAge}</p> }
-    {getLocation(user.userLocation)}
+    <h1>Count: {count}</h1>
+    <button onClick={addOne}>+1</button>
+    <button onClick={minusOne}>-1</button>
+    <button onClick={reset}>Reset</button>
     </div>
 );
+console.log(templateTwo);
 
-// I --> create app object title/subtitle
-//use title/subtitle in the template
-//render template
-
-// II --> only render the subtitle (and p tag) if subtitle exist - logical and operator
-// render new p tag - if options.legth > 0 "Here are your options" "No options"
-
-// III --> Use const if we are never reasighn
-// Only wen we reasighn veriable then use let 
-const app = {
-    title: 'The Matrix',
-    subtitles: 'Put your life in the hands of a computer',
-    options: ['One Option', 'Two Options']
-}
-
-const templateApp = (
-    <div>
-        <h1>The Movie is: {app.title}</h1> 
-        <p>Subtitles: {app.subtitles}</p> 
-        {app.subtitles && <p>{app.subtitles}</p>}
-        <p>{app.options.length > 0 ? 'Here are your options' : 'No Options'}</p>
-        <ol>
-        <li>Item One</li>
-        <li>Item Two</li>
-        </ol>
-    </div>);
+//create two new button
+// Make Button "-1" set up minusOne() and register - log 'minusOne'
+// Make Reset buttor "reset" - setup reset() log "reset"
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(templateApp, appRoot);
+ReactDOM.render(templateTwo, appRoot);
