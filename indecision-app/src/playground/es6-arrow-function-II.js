568c533b0275a8ecd39ab5771ec2167b0dd941a5
Vladimir Stratiev <vladistratiev@vladis-mac.local>
2021-01-27 15:39:09 +0200

    ES6 Aside: Arrow Functions II

--- indecision-app/src/playground/es6-arrow-function-II.js
+++ indecision-app/src/playground/es6-arrow-function-II.js
@@ -1,28 +1,23 @@
-'use strict';
-
 // argument object - no longer bound with arrow functions
 
 // const add = function(a, b) {
 //     console.log(arguments)
 //     return a + b;
 // }
-var add = function add(a, b) {
+const add = (a,b) => {
     //console.log(arguments); no longer used
     return a + b;
-};
-console.log(add(55, 1, 1001));
+}
+console.log(add(55,1, 1001));
+
 
 // this keyword - no longer bound
-var user = {
+const user = {
     name: 'Vladimir',
     cities: ['Varna', 'Alaska', 'London', 'Sofia'],
-    printPlacesLived: function printPlacesLived() {
-        var _this = this;
-
-        return this.cities.map(function (city) {
-            return _this.name + ' has lived in ' + city;
-        });
-        //printPlacesLived: function(){
+    printPlacesLived() {
+        return this.cities.map((city) => this.name + ' has lived in ' + city);
+    //printPlacesLived: function(){
         // console.log(this.name);
         // console.log(this.cities);
         // const that = this; // using this as parents function
@@ -39,19 +34,15 @@ var user = {
 console.log(user.printPlacesLived());
 
 //
-var multiplier = {
+const multiplier = {
     // array of numbers
     // multiplyBy - single number
     // multiply - return a new array where the numbers have been multiplied
-    numbers: [10, 20, 30],
+    numbers: [10,20,30],
     multiplyBy: 3,
-    multiply: function multiply() {
-        var _this2 = this;
-
-        return this.numbers.map(function (number) {
-            return number * _this2.multiplyBy;
-        });
+    multiply() {
+        return this.numbers.map((number) => number * this.multiplyBy);
     }
-};
+}
 
 console.log(multiplier.multiply()); //[1,2,3] 2 [2,4,6]
