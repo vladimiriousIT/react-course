116c5e9e95d225ee716c6ec51b17e86c1fe49900
Vladimir Stratiev <vladistratiev@vladis-mac.local>
2021-01-27 16:15:47 +0200

    Manual Data Binding

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -2,62 +2,51 @@
 
 console.log('App.js is Running!');
 
-// const templateApp = (
-//     <div>
-//         <h1>The Movie is: {app.title}</h1> 
-//         <p>Subtitles: {app.subtitles}</p> 
-//         {app.subtitles && <p>{app.subtitles}</p>}
-//         <p>{app.options.length > 0 ? 'Here are your options' : 'No Options'}</p>
-//         <ol>
-//         <li>Item One</li>
-//         <li>Item Two</li>
-//         </ol>
-//     </div>);
-
 var count = 0;
 var addOne = function addOne() {
-    console.log('plusOne');
+    count++; // count = count + 1;
+    renderCounterApp();
 };
 
 var minusOne = function minusOne() {
-    console.log('minusOne');
+    count--;
+    renderCounterApp();
 };
 
 var reset = function reset() {
-    console.log('reset');
+    count = 0;
+    renderCounterApp();
 };
-
-var templateTwo = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        'Count: ',
-        count
-    ),
-    React.createElement(
-        'button',
-        { onClick: addOne },
-        '+1'
-    ),
-    React.createElement(
-        'button',
-        { onClick: minusOne },
-        '-1'
-    ),
-    React.createElement(
-        'button',
-        { onClick: reset },
-        'Reset'
-    )
-);
-console.log(templateTwo);
-
-//create two new button
-// Make Button "-1" set up minusOne() and register - log 'minusOne'
-// Make Reset buttor "reset" - setup reset() log "reset"
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+var renderCounterApp = function renderCounterApp() {
+    var templateTwo = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            'Count: ',
+            count
+        ),
+        React.createElement(
+            'button',
+            { onClick: addOne },
+            '+1'
+        ),
+        React.createElement(
+            'button',
+            { onClick: minusOne },
+            '-1'
+        ),
+        React.createElement(
+            'button',
+            { onClick: reset },
+            'Reset'
+        )
+    );
+    ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,44 +1,33 @@
 console.log('App.js is Running!');
 
-// const templateApp = (
-//     <div>
-//         <h1>The Movie is: {app.title}</h1> 
-//         <p>Subtitles: {app.subtitles}</p> 
-//         {app.subtitles && <p>{app.subtitles}</p>}
-//         <p>{app.options.length > 0 ? 'Here are your options' : 'No Options'}</p>
-//         <ol>
-//         <li>Item One</li>
-//         <li>Item Two</li>
-//         </ol>
-//     </div>);
-
 let count = 0;
 const addOne = () => {
-    console.log('plusOne');
+    count++; // count = count + 1;
+    renderCounterApp();
 };
 
 const minusOne = () => {
-    console.log('minusOne');
+    count--;
+    renderCounterApp();
 };
 
 const reset = () => {
-    console.log('reset');
+    count = 0;
+    renderCounterApp();
 };
-
-const templateTwo = (
-    <div>
-    <h1>Count: {count}</h1>
-    <button onClick={addOne}>+1</button>
-    <button onClick={minusOne}>-1</button>
-    <button onClick={reset}>Reset</button>
-    </div>
-);
-console.log(templateTwo);
-
-//create two new button
-// Make Button "-1" set up minusOne() and register - log 'minusOne'
-// Make Reset buttor "reset" - setup reset() log "reset"
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+const renderCounterApp = () => {
+    const templateTwo = (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={addOne}>+1</button>
+            <button onClick={minusOne}>-1</button>
+            <button onClick={reset}>Reset</button>
+        </div>
+    );
+    ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
